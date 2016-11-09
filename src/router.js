c2c507a815317e5e22181b31d9e797ba15e44e37
lucymonie <lucy.monie.hall@gmail.com>
2016-11-09 18:06:25 +0000

    adds tests to check if istanbul is working as expected

---
+++ src/router.js
@@ -0,0 +1,26 @@
+function add (a,b) {
+  var result = a + b;
+  return result;
+}
+
+function subtract (a,b) {
+  var result = a - b;
+  return result;
+}
+
+function multiply (a,b) {
+  var result = a * b;
+  return result;
+}
+
+function divide (a,b) {
+  var result = a / b;
+  return result;
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide
+}
