89026e845c678ab8ba85013d77ad65641cebb03e
jsms90 <jsms@hotmail.co.uk>
2016-11-10 17:39:18 +0000

    make post request its own module

--- public/main.js
+++ public/main.js
@@ -7,26 +7,6 @@ inputField.addEventListener('input', function () {
   makePostRequest(endpoint, contents, handleResponse);
 });
 
-function makePostRequest (url, data, cb) {
-  var httpRequest = new XMLHttpRequest();
-  if (!httpRequest) {
-    alert('Cannot create a http request.');
-    return false;
-  }
-  httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        cb(null, httpRequest.response);
-      } else {
-        cb('There was a problem with the request');
-      }
-    }
-  };
-  httpRequest.open('POST', url);
-  httpRequest.setRequestHeader('content-type', 'text/xml');
-  httpRequest.send(data);
-}
-
 function handleResponse (err, response) {
   if (err) {
     console.log(err);
