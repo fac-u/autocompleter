b619c41a610e199c0cab3e796fa4b3370ff9cfc5
lucymonie <lucy.monie.hall@gmail.com>
2016-11-10 15:36:55 +0000

    add handler object for router build

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,13 @@
-module.exports = handler;
-
-function handler(request, response){
+var handler = {
+  index: function(req, res){
+		res.handler = 'index';
+	},
+	getSuggestions: function(req, res) {
+    res.handler = 'getSuggestions';
+	},
+	notFound: function(req,res) {
+	  res.handler = 'notFound';
+	}
 }
+
+module.exports = handler
