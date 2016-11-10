fe409ea308cfdafdbc4b1fafc1f230f79531fba4
jsms90 <jsms@hotmail.co.uk>
2016-11-10 18:09:04 +0000

    refactor main.js to remove superfluous handler functions

--- public/main.js
+++ public/main.js
@@ -3,25 +3,17 @@ var inputField = document.getElementById('searchBox');
 inputField.addEventListener('input', function () {
   var contents = inputField.value;
   var endpoint = '/get_suggestions';
-  request.post(endpoint, contents, handleResponse);
-});
-
-function handleResponse (err, response) {
-  if (err) {
-    console.log(err, 'Status code:', response);
-    return;
-  }
-  updateDom(response);
-}
-
-function updateDom (res) {
-  var arr = JSON.parse(res);
-  var ul = document.createElement('ul');
-  ul.className = 'results';
-  arr.forEach(function (element) {
-    var li = document.createElement('li');
-    li.textContent = element;
-    ul.appendChild(li);
-  });
-  document.getElementById('leftAlign').appendChild(ul);
+  request.post(endpoint, contents, function (err, res) {
+    if (err) {
+      return console.log(err, 'Status code:', response);
+    }
+    var arr = JSON.parse(res);
+    var ul = document.createElement('ul');
+    ul.className = 'results';
+    arr.forEach(function (element) {
+      var li = document.createElement('li');
+      li.textContent = element;
+      ul.appendChild(li);
+    });
+    document.getElementById('leftAlign').appendChild(ul);
 }
