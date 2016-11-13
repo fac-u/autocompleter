329372b00cac9f6a1d2db2557930c07cf22dbefa
WilliamSavage <williamsavage@hotmail.com>
2016-11-13 19:04:43 +0000

    some styling added to click update inputfield for UX

--- public/main.js
+++ public/main.js
@@ -1,8 +1,9 @@
 var inputField = document.getElementById('searchBox');
 
+// sends request for popular words every input and updates the dom
 inputField.addEventListener('input', function () {
   var contents = inputField.value;
-  var lastWord = contents.split(' ').pop();
+  var lastWord = contents.split(' ').pop().toLowerCase();
   postRequest(lastWord);
 });
 
@@ -17,9 +18,23 @@ function postRequest (str) {
   });
 }
 
+function updateDom (res) {
+  var arr = JSON.parse(res);
+  var newUl = document.createElement('ul');
+  arr.forEach(function (element) {
+    var li = document.createElement('li');
+    li.className = 'list';
+    li.tabIndex = 0;
+    li.textContent = element;
+    newUl.appendChild(li);
+  });
+  document.getElementsByClassName('results')[0].innerHTML = newUl.innerHTML;
+}
+
+// populates input field with clicked suggested word and clears suggestions
 document.querySelector('ul').addEventListener('click', function (event) {
   if (event.target.tagName.toLowerCase() === 'li') {
-    event.target.style.color = '#000';
+    event.target.style.color = '#1b424f';
     var currentWord = event.target.textContent;
     var words = inputField.value.split(' ').slice(0, -1);
     words.push(currentWord);
@@ -28,18 +43,6 @@ document.querySelector('ul').addEventListener('click', function (event) {
     inputField.focus();
     setTimeout(function () {
       document.getElementsByClassName('results')[0].innerHTML = null;
-    }, 200);
+    }, 220);
   }
 });
-
-function updateDom (res) {
-  var arr = JSON.parse(res);
-  var newUl = document.createElement('ul');
-  arr.forEach(function (element) {
-    var li = document.createElement('li');
-    li.className = "list";
-    li.textContent = element;
-    newUl.appendChild(li);
-  });
-  document.getElementsByClassName('results')[0].innerHTML = newUl.innerHTML;
-}
