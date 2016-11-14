01dfc263d91f00debd6402049076cc7926b2c11c
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-11-14 15:16:49 +0000

    Tweak code syntax into eslint-semistandard style

--- fTest/fTest.js
+++ fTest/fTest.js
@@ -1,8 +1,3 @@
 QUnit.test('hello test', function (assert) {
   assert.ok(true, 'Passed!');
 });
-/*
-QUnit.test('Check input', function (assert) {
-  assert.equal(document.getElementById('searchBox').getElementType === INPUT, 'Passed!');
-});
-*/
