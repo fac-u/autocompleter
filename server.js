f6b0c1f74960016fd3c2b06ffa71a27689408580
lucymonie <lucy.monie.hall@gmail.com>
2016-11-14 11:35:58 +0000

    Adds code to server.js to ping the app every five minutes to stop it from sleeping. Torturing out own app, but hey Trump says it's okay

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ var server = http.createServer(router);
 server.listen(port);
 
 console.log('FACU server is listening on port ' + port + ' and is ready for requests.');
+
+setInterval(function() {
+    http.get("http://facu-autocompleter.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
